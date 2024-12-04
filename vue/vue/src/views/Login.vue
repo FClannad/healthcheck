7047726979a9c751b813238f6d983e1615ff84d0
sakura <2692433610@qq.com>
2024-12-04 20:53:10 +0800

    new

---
+++ vue/vue/src/views/Login.vue
@@ -0,0 +1,313 @@
+<template>
+  <div class="loginVideo">
+    <video src="../assets/video/read.mp4" autoPlay muted :loop="true" :controls="false" preload="auto" />
+    <div class="loginBox">
+      <!-- 登录框 -->
+      <div class="container">
+        <div class="drop">
+          <div class="content">
+            <h2 :style="{ color: '#00000' }" >体检管理系统</h2>
+            <el-form ref="formRef" :model="data.form" :rules="data.rules">
+              <div class="inputBox" >
+                <el-input :prefix-icon="User" size="large" v-model="data.form.username" placeholder="输入账号"></el-input>
+              </div>
+              <div class="inputBox">
+                <el-input show-password :prefix-icon="Lock" size="large" v-model="data.form.password" placeholder="输入密码"></el-input>
+              </div>
+
+              <el-form-item prop="role">
+                <el-select  v-model="data.form.role" style="width: 60%; margin-left: 20%;  ">
+                  <el-option value="ADMIN" label="管理员"></el-option>
+                  <el-option value="DOCTOR" label="医生"></el-option>
+                  <el-option value="USER" label="用户"></el-option>
+                </el-select>
+              </el-form-item>
+
+
+              <el-form-item>
+                <el-button size="large" type="primary" style="width: 70%; margin-left: 15%; margin-bottom:-27px" @click="login">登 录</el-button>
+              </el-form-item>
+
+              <div >
+                还没有账号？请 <a href="/register">注册</a>
+              </div>
+
+            </el-form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import { reactive, ref } from "vue";
+import { User, Lock } from "@element-plus/icons-vue";
+import request from "@/utils/request.js";
+import {ElMessage} from "element-plus";
+import router from "@/router/index.js";
+
+
+const data = reactive({
+  form: { role: 'USER' },
+  rules: {
+    username: [
+      { required: true, message: '请输入账号', trigger: 'blur' }
+    ],
+    password: [
+      { required: true, message: '请输入密码', trigger: 'blur' }
+    ]
+  }
+})
+
+const formRef = ref()
+
+const login = () => {
+  formRef.value.validate(valid => {
+    if (valid) {
+      // 表示表单校验通过
+      console.log('发送的登录请求数据:', data.form);
+      request.post('/login', data.form).then(res => {
+
+        if (res.code === '200') {
+          ElMessage.success('登录成功')
+          // 存储用户信息到浏览器的缓存
+          localStorage.setItem('xm-user', JSON.stringify(res.data))
+
+          setTimeout(() => {
+            if (res.data.role === 'ADMIN') {
+              location.href = '/manager/dataAnalysis'
+            } else {
+              location.href = '/manager/home'
+            }
+          }, 500)
+        }
+        else {
+          ElMessage.error(res.msg)
+        }
+      })
+    }
+  })
+}
+</script>
+<style scoped>
+.loginVideo {
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
+}
+
+/* 验证码样式 */
+.canvascs {
+  width: 92px;
+  height: 30px;
+  border-radius: 5px;
+  margin-top: 2px;
+  background: white;
+  margin-left: 5px;
+}
+
+#yanzheng {
+  display: flex;
+  width: 130px;
+}
+
+#yanzheng div {
+  width: 96px;
+  /* background: pink; */
+}
+
+#yanzheng input {
+  width: 100px;
+}
+
+video {
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+  object-position: center;
+}
+
+.loginBox {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+/* 登录框 */
+.container .drop {
+  position: relative;
+  width: 350px;
+  height: 350px;
+  box-shadow: inset 20px 20px 20px rgba(0, 0, 0, 0.05),
+  25px 35px 20px rgba(0, 0, 0, 0.05), 25px 30px 30px rgba(0, 0, 0, 0.05),
+  inset -20px -20px 25px rgba(255, 255, 255, 0.9);
+  transition: 0.5s;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+}
+
+.container .drop:hover {
+  border-radius: 10%;
+}
+
+.container .drop::before {
+  content: "";
+  position: absolute;
+  top: 50px;
+  left: 85px;
+  width: 35px;
+  height: 35px;
+  border-radius: 50%;
+  background: #fff;
+  opacity: 0.9;
+}
+
+.container .drop::after {
+  content: "";
+  position: absolute;
+  top: 90px;
+  left: 110px;
+  width: 15px;
+  height: 15px;
+  border-radius: 50%;
+  background: #fff;
+  opacity: 0.9;
+}
+
+.container .drop .content {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  /* align-items: center; */
+  flex-direction: column;
+  text-align: center;
+  gap: 15px;
+}
+
+.container .drop .content h2 {
+  position: relative;
+  color: #333;
+  font-size: 1.5em;
+}
+
+.container .drop .content form {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  justify-content: center;
+  /* align-items: center; */
+}
+
+.container .drop .content form .inputBox {
+  position: relative;
+  width: 225px;
+  box-shadow: inset 2px 5px 10px rgba(0, 0, 0, 0.1),
+  inset -2px -5px 10px rgba(255, 255, 255, 1),
+  15px 15px 10px rgba(0, 0, 0, 0.05), 15px 10px 15px rgba(0, 0, 0, 0.025);
+  border-radius: 25px;
+}
+
+.container .drop .content form .inputBox::before {
+  content: "";
+  position: absolute;
+  top: 8px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 65%;
+  height: 5px;
+  background: rgba(255, 255, 255, 0.5);
+  border-radius: 5px;
+}
+
+.container .drop .content form .inputBox input {
+  border: none;
+  outline: none;
+  background: transparent;
+  width: 100%;
+  font-size: 1em;
+  padding: 10px 5px;
+}
+
+.container .drop .content form .inputBox input[type="submit"] {
+  color: #fff;
+  text-transform: uppercase;
+  font-size: 1em;
+  cursor: pointer;
+  letter-spacing: 0.1em;
+  font-weight: 500;
+}
+
+.container .drop .content form .inputBox:last-child {
+  width: 120px;
+  background: #3399ff;
+  box-shadow: inset 2px 5px 10px rgba(0, 0, 0, 0.1),
+  15px 15px 10px rgba(0, 0, 0, 0.05), 15px 10px 15px rgba(0, 0, 0, 0.025);
+  transition: 0.5s;
+}
+
+.container .drop .content form .inputBox:last-child:hover {
+  width: 150px;
+}
+
+.btns {
+  position: absolute;
+  right: -120px;
+  bottom: 0;
+  width: 120px;
+  height: 120px;
+  background: #00a6bc;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  text-decoration: none;
+  color: #fff;
+  line-height: 1.2em;
+  letter-spacing: 0.1em;
+  font-size: 0.8em;
+  transition: 0.25s;
+  text-align: center;
+  box-shadow: inset 10px 10px 10px rgba(0, 166, 188, 0.05),
+  15px 25px 10px rgba(0, 166, 188, 0.1), 15px 20px 20px rgba(0, 166, 188, 0.1),
+  inset -10px -10px 15px rgba(0, 166, 188, 0.5);
+  border-radius: 50%;
+}
+
+.btns::before {
+  content: "";
+  position: absolute;
+  top: 15px;
+  left: 30px;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background: #fff;
+  opacity: 0.45;
+}
+
+.btns.signup {
+  bottom: 150px;
+  right: -120px;
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+  background: #00a6bc;
+  box-shadow: inset 10px 10px 10px rgba(0, 166, 188, 0.05),
+  15px 25px 10px rgba(0, 166, 188, 0.1), 15px 20px 20px rgba(0, 166, 188, 0.1),
+  inset -10px -10px 15px rgba(0, 166, 188, 0.5);
+}
+
+.btns.signup::before {
+  left: 20px;
+  width: 15px;
+  height: 15px;
+}
+
+.btns:hover {
+  border-radius: 10%;
+}
+</style>
---
+++ vue/vue/src/views/manager/Person.vue
@@ -0,0 +1,120 @@
+<template>
+  <div style="width: 50%" class="card">
+    <el-form ref="user" :model="data.user" label-width="70px" style="padding: 20px">
+      <el-form-item prop="avatar" label="头像">
+        <el-upload
+            :action="baseUrl + '/files/upload'"
+            :on-success="handleFileUpload"
+            :show-file-list="false"
+            class="avatar-uploader"
+        >
+          <img v-if="data.user.avatar" :src="data.user.avatar" class="avatar" />
+          <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+        </el-upload>
+      </el-form-item>
+      <el-form-item prop="username" label="用户名">
+        <el-input disabled v-model="data.user.username" placeholder="请输入用户名"></el-input>
+      </el-form-item>
+      <el-form-item prop="name" label="姓名">
+        <el-input v-model="data.user.name" placeholder="请输入姓名"></el-input>
+      </el-form-item>
+      <el-form-item prop="phone" label="电话">
+        <el-input v-model="data.user.phone" placeholder="请输入电话"></el-input>
+      </el-form-item>
+      <el-form-item prop="email" label="邮箱">
+        <el-input v-model="data.user.email" placeholder="请输入邮箱"></el-input>
+      </el-form-item>
+      <el-form-item prop="workTime" label="从业时长" v-if="data.user.role === 'DOCTOR'">
+        <el-input v-model="data.user.workTime" placeholder="请输入从业时长"></el-input>
+      </el-form-item>
+      <el-form-item prop="descr" label="个人介绍" v-if="data.user.role === 'DOCTOR'">
+        <el-input type="textarea" :rows="6" v-model="data.user.descr" placeholder="请输入个人介绍"></el-input>
+      </el-form-item>
+      <div style="text-align: center">
+        <el-button type="primary" @click="update">保 存</el-button>
+      </div>
+    </el-form>
+  </div>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+import request from "@/utils/request.js";
+import {ElMessage} from "element-plus";
+
+const baseUrl = import.meta.env.VITE_BASE_URL
+
+const data = reactive({
+  user: JSON.parse(localStorage.getItem('xm-user') || '{}')
+})
+
+const handleFileUpload = (res) => {
+  data.user.avatar = res.data
+}
+
+const emit = defineEmits(['updateUser'])
+const update = () => {
+  if (data.user.role === 'ADMIN') {
+    request.put('/admin/update', data.user).then(res => {
+      if (res.code === '200') {
+        ElMessage.success('保存成功')
+        localStorage.setItem('xm-user', JSON.stringify(data.user))
+        emit('updateUser')
+      } else {
+        ElMessage.error(res.msg)
+      }
+    })
+  } else if (data.user.role === 'DOCTOR') {
+    request.put('/doctor/update', data.user).then(res => {
+      if (res.code === '200') {
+        ElMessage.success('保存成功')
+        localStorage.setItem('xm-user', JSON.stringify(data.user))
+        emit('updateUser')
+      } else {
+        ElMessage.error(res.msg)
+      }
+    })
+  } else if (data.user.role === 'USER') {
+    request.put('/user/update', data.user).then(res => {
+      if (res.code === '200') {
+        ElMessage.success('保存成功')
+        localStorage.setItem('xm-user', JSON.stringify(data.user))
+        emit('updateUser')
+      } else {
+        ElMessage.error(res.msg)
+      }
+    })
+  }
+}
+</script>
+
+<style>
+.avatar-uploader {
+  height: 120px;
+}
+.avatar-uploader .avatar {
+  width: 120px;
+  height: 120px;
+  display: block;
+}
+.avatar-uploader .el-upload {
+  border: 1px dashed var(--el-border-color);
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  transition: var(--el-transition-duration-fast);
+}
+
+.avatar-uploader .el-upload:hover {
+  border-color: var(--el-color-primary);
+}
+
+.el-icon.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 120px;
+  height: 120px;
+  text-align: center;
+}
+</style>
